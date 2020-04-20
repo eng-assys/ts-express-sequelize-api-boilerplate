bb9748d03a6f642e2de1a0561f45fe89964216ed
Lucas Assis <eng.assys@gmail.com>
2020-04-20 14:53:08 +0000

    first commit

---
+++ lib/config/routes.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express"
+import { NodesController } from "../controllers/nodes.controller"
+import { LinksController } from "../controllers/links.controller"
+
+export class Routes {
+  public nodesController: NodesController = new NodesController()
+  public linksController: LinksController = new LinksController()
+
+  public routes(app): void {
+
+    // Nodes endpoints
+    app
+      .route("/nodes")
+      .get(this.nodesController.index)
+      .post(this.nodesController.create)
+    
+    app
+      .route("/nodes/:id")
+      .get(this.nodesController.show)
+      .put(this.nodesController.update)
+      .delete(this.nodesController.delete)
+
+    // Links Endpoints
+    app
+      .route("/links")
+      .get(this.linksController.index)
+      .post(this.linksController.create)
+
+    app
+      .route("/links/:id")
+      .get(this.linksController.show)
+      .put(this.linksController.update)
+      .delete(this.linksController.delete)
+  }
+}
